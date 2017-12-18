7b487326a2854a6d0fc3ae325281ffab484ddc7f
Hexah <hexer504@gmail.com>
2017-12-18 19:47:11 +0100

    travis

--- ci/build.sh
+++ ci/build.sh
@@ -8,12 +8,8 @@ tar -xzf sourcemod.tar.gz
 echo "Give compiler rights for compile"
 chmod +x addons/sourcemod/scripting/spcomp
 
-echo "Compile MyJailbreak plugins"
-for file in addons/sourcemod/scripting/*.sp
-do
-echo "Compile $file"
-  addons/sourcemod/scripting/spcomp -E -v0 $file
-done
+
+addons/sourcemod/scripting/spcomp -E -v0 plugin.sp
 
 echo "Remove plugins folder if exists"
 if [ -d "addons/sourcemod/plugins" ]; then
@@ -22,16 +18,5 @@ fi
 
 echo "Create clean plugins folder"
 mkdir addons/sourcemod/plugins
-
-echo "Remove build folder if exists"
-if [ -d "build" ]; then
-  rm -r build
-fi
-
-echo "Clean root folder"
-rm sourcemod.tar.gz
-rm translations.zip
-
-
 
 echo "Build done"
